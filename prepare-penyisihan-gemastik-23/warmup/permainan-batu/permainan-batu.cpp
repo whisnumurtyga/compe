01660a267de9f918e4a6edb8a8017ec01bb9eaf2
whisnumurtyga <whisnumurty.galih.21@student.if.ittelkom-sby.ac.id>
2023-07-19 22:20:58 +0700

    nim sum fix

--- prepare-penyisihan-gemastik-23/warmup/permainan-batu/permainan-batu.cpp
+++ prepare-penyisihan-gemastik-23/warmup/permainan-batu/permainan-batu.cpp
@@ -7,11 +7,12 @@ using namespace std;
 // Fungsi untuk menghitung nim-sum dari suatu bilangan
 long long nim_sum(long long n) {
     switch (n % 4) {
-    case 0: return n;          // Jika n % 4 = 0, nim-sum adalah n
-    case 1: return 1;          // Jika n % 4 = 1, nim-sum adalah 1
-    case 2: return n + 1;      // Jika n % 4 = 2, nim-sum adalah n + 1
+    // case 0: return n;          // Jika n % 4 = 0, nim-sum adalah n
+    // case 1: return 1;          // Jika n % 4 = 1, nim-sum adalah 1
+    // case 2: return n + 1;      // Jika n % 4 = 2, nim-sum adalah n + 1
     case 3: return 0;          // Jika n % 4 = 3, nim-sum adalah 0
-    }
+    default: return n;
+    } 
     return 0; // Baris ini tidak akan pernah tercapai, hanya untuk menghindari peringatan kompiler
 }
 
